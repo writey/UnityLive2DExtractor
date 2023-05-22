0c6953a254909227acf7378f3d4787b748415637
writey <x18752457230@gmail.com>
2023-05-22 17:34:39 +0800

    memento-mori

--- UnityLive2DExtractor/Program.cs
+++ UnityLive2DExtractor/Program.cs
@@ -74,7 +74,7 @@ namespace UnityLive2DExtractor
                 var basePath = container.Substring(0, container.LastIndexOf("/"));
                 basePathList.Add(basePath);
             }
-            var lookup = containers.ToLookup(x => basePathList.Find(b => x.Value.Contains(b)), x => x.Key);
+            var lookup = containers.ToLookup(x => basePathList.Find(b => x.Value.Contains(b) || x.Value.Contains(b.Replace("/model", "/motion"))), x => x.Key);
             var baseDestPath = Path.Combine(Path.GetDirectoryName(args[0]), "Live2DOutput");
             foreach (var assets in lookup)
             {
